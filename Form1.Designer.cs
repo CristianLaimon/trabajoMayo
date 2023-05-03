04a8d22189e3f93eb65e3e497c66b413f1eefc63
Kristan Ruíz <itan_07@hotmail.com>
2023-05-03 09:12:51 -0800

    Agregando las flechas

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -30,12 +30,17 @@
         {
             this.panel1 = new System.Windows.Forms.Panel();
             this.panel2 = new System.Windows.Forms.Panel();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
-            this.button4 = new System.Windows.Forms.Button();
+            this.buttonCrearTrapecio = new System.Windows.Forms.Button();
+            this.buttonCrearTriangulo = new System.Windows.Forms.Button();
+            this.buttonCrearRombo = new System.Windows.Forms.Button();
+            this.buttonCrearHexagono = new System.Windows.Forms.Button();
             this.panel3 = new System.Windows.Forms.Panel();
             this.label1 = new System.Windows.Forms.Label();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.buttonMoverArriba = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
             this.panel3.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -54,48 +59,48 @@
             this.panel2.Size = new System.Drawing.Size(644, 363);
             this.panel2.TabIndex = 1;
             // 
-            // button1
+            // buttonCrearTrapecio
             // 
-            this.button1.Location = new System.Drawing.Point(180, 28);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(116, 48);
-            this.button1.TabIndex = 2;
-            this.button1.Text = "Trapecio";
-            this.button1.UseVisualStyleBackColor = true;
+            this.buttonCrearTrapecio.Location = new System.Drawing.Point(180, 28);
+            this.buttonCrearTrapecio.Name = "buttonCrearTrapecio";
+            this.buttonCrearTrapecio.Size = new System.Drawing.Size(116, 48);
+            this.buttonCrearTrapecio.TabIndex = 2;
+            this.buttonCrearTrapecio.Text = "Trapecio";
+            this.buttonCrearTrapecio.UseVisualStyleBackColor = true;
             // 
-            // button2
+            // buttonCrearTriangulo
             // 
-            this.button2.Location = new System.Drawing.Point(320, 28);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(116, 48);
-            this.button2.TabIndex = 3;
-            this.button2.Text = "Triángulo";
-            this.button2.UseVisualStyleBackColor = true;
+            this.buttonCrearTriangulo.Location = new System.Drawing.Point(320, 28);
+            this.buttonCrearTriangulo.Name = "buttonCrearTriangulo";
+            this.buttonCrearTriangulo.Size = new System.Drawing.Size(116, 48);
+            this.buttonCrearTriangulo.TabIndex = 3;
+            this.buttonCrearTriangulo.Text = "Triángulo";
+            this.buttonCrearTriangulo.UseVisualStyleBackColor = true;
             // 
-            // button3
+            // buttonCrearRombo
             // 
-            this.button3.Location = new System.Drawing.Point(458, 28);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(116, 48);
-            this.button3.TabIndex = 4;
-            this.button3.Text = "Rombo";
-            this.button3.UseVisualStyleBackColor = true;
+            this.buttonCrearRombo.Location = new System.Drawing.Point(458, 28);
+            this.buttonCrearRombo.Name = "buttonCrearRombo";
+            this.buttonCrearRombo.Size = new System.Drawing.Size(116, 48);
+            this.buttonCrearRombo.TabIndex = 4;
+            this.buttonCrearRombo.Text = "Rombo";
+            this.buttonCrearRombo.UseVisualStyleBackColor = true;
             // 
-            // button4
+            // buttonCrearHexagono
             // 
-            this.button4.Location = new System.Drawing.Point(604, 28);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(116, 48);
-            this.button4.TabIndex = 5;
-            this.button4.Text = "Hexágono";
-            this.button4.UseVisualStyleBackColor = true;
+            this.buttonCrearHexagono.Location = new System.Drawing.Point(604, 28);
+            this.buttonCrearHexagono.Name = "buttonCrearHexagono";
+            this.buttonCrearHexagono.Size = new System.Drawing.Size(116, 48);
+            this.buttonCrearHexagono.TabIndex = 5;
+            this.buttonCrearHexagono.Text = "Hexágono";
+            this.buttonCrearHexagono.UseVisualStyleBackColor = true;
             // 
             // panel3
             // 
-            this.panel3.Controls.Add(this.button3);
-            this.panel3.Controls.Add(this.button4);
-            this.panel3.Controls.Add(this.button1);
-            this.panel3.Controls.Add(this.button2);
+            this.panel3.Controls.Add(this.buttonCrearRombo);
+            this.panel3.Controls.Add(this.buttonCrearHexagono);
+            this.panel3.Controls.Add(this.buttonCrearTrapecio);
+            this.panel3.Controls.Add(this.buttonCrearTriangulo);
             this.panel3.Location = new System.Drawing.Point(0, 490);
             this.panel3.Name = "panel3";
             this.panel3.Size = new System.Drawing.Size(937, 105);
@@ -110,11 +115,61 @@
             this.label1.TabIndex = 7;
             this.label1.Text = "Crear Figura:";
             // 
+            // button5
+            // 
+            this.button5.Image = global::trabajoMayo.Properties.Resources.arrow1;
+            this.button5.Location = new System.Drawing.Point(362, 669);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(58, 52);
+            this.button5.TabIndex = 8;
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            this.button6.Image = global::trabajoMayo.Properties.Resources.right_arrow1;
+            this.button6.Location = new System.Drawing.Point(496, 669);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(58, 52);
+            this.button6.TabIndex = 9;
+            this.button6.UseVisualStyleBackColor = true;
+            // 
+            // buttonMoverArriba
+            // 
+            this.buttonMoverArriba.Image = global::trabajoMayo.Properties.Resources.up_arrow1;
+            this.buttonMoverArriba.Location = new System.Drawing.Point(432, 613);
+            this.buttonMoverArriba.Name = "buttonMoverArriba";
+            this.buttonMoverArriba.Size = new System.Drawing.Size(58, 52);
+            this.buttonMoverArriba.TabIndex = 10;
+            this.buttonMoverArriba.UseVisualStyleBackColor = true;
+            // 
+            // button8
+            // 
+            this.button8.Image = global::trabajoMayo.Properties.Resources.down_arrow1;
+            this.button8.Location = new System.Drawing.Point(432, 722);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(58, 52);
+            this.button8.TabIndex = 11;
+            this.button8.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(211, 617);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(89, 20);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "Movimiento";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(937, 775);
+            this.ClientSize = new System.Drawing.Size(937, 795);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.buttonMoverArriba);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.panel3);
             this.Controls.Add(this.panel2);
@@ -131,11 +186,16 @@
 
         private Panel panel1;
         private Panel panel2;
-        private Button button1;
-        private Button button2;
-        private Button button3;
-        private Button button4;
+        private Button buttonCrearTrapecio;
+        private Button buttonCrearTriangulo;
+        private Button buttonCrearRombo;
+        private Button buttonCrearHexagono;
         private Panel panel3;
         private Label label1;
+        private Button button5;
+        private Button button6;
+        private Button buttonMoverArriba;
+        private Button button8;
+        private Label label2;
     }
 }
