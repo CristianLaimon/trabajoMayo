8223c694233f1ba027977b9018c2bc89579a4e98
Kristan Ruíz <itan_07@hotmail.com>
2023-05-03 09:22:00 -0800

    (codigo) Clase figura creada

---
+++ Figura.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace trabajoMayo
+{
+    internal abstract class Figura
+    {
+        private int lado, perimetro;
+        private double area;
+  
+        public Figura()
+        {
+            lado = 0;
+            perimetro = 0;
+            area = 0;
+        }
+
+        public Figura(int lado, int perimetro)
+        {
+            this.lado = lado;
+            this.perimetro = perimetro;
+            area = 0;
+        }
+
+        public abstract double CalcularArea();
+        public abstract int CalcularPerimetro();
+    }
+}
